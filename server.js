fc9ebc3c098f1766838b66810cfdaa8dadf98a29
andiniseptia <andiniseptiandri124@gmail.com>
2024-11-23 23:24:00 +0700

    Menambahkan body-parser middleware dan memulai server di port 5000

--- server.js
+++ server.js
@@ -4,3 +4,7 @@ import bodyParser from 'body-parser'
 const app = express();
 const PORT = 5000;
 
+app.use(bodyParser.json());
+app.listen(PORT, () =>
+    console.log(`Server berjalan di port : http://localhost:${PORT}`)
+);
