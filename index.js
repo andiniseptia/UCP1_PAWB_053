4feb9a39a6ffccd79edc39d0b5e766ba6db667b8
andiniseptia <andiniseptiandri124@gmail.com>
2024-11-23 23:21:40 +0700

    Menambahkan konfigurasi middleware dan routing untuk bibit dan pupuk

--- index.js
+++ index.js
@@ -6,3 +6,17 @@ import pupukRoute from "./routes/pupuk.js";
 const app = express();
 const PORT = 5000;
 
+app.use(express.json());
+app.use(bodyParser.json());
+
+app.use("/bibit", bibitRoute);
+app.use("/pupuk", pupukRoute);
+
+app.get("/", (req, res) => {
+    res.send("Selamat Pagi");
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server berjalan di port : http://localhost:${PORT}`)
+});
