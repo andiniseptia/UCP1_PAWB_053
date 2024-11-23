66d6bce2b0911ab614a9936aba9993b8f1f95182
andiniseptia <andiniseptiandri124@gmail.com>
2024-11-23 23:22:58 +0700

    Menambahkan view engine EJS dan routing untuk bibit, pupuk, dan todos

--- app.js
+++ app.js
@@ -6,4 +6,15 @@ const pupukRoute = require("./routes/pupuk.js"); // Menggunakan require
 
 const port = 5000;
 
+app.use(express.json());
+app.use("/bibit", bibitRoute);
+app.use("/pupuk", pupukRoute);
+app.use('/todos', todoRoutes);
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => {
+    res.render('index');
+});
 
+app.listen(port, () =>
+    console.log(`Server berjalan di port : http://localhost:${port}`)
+);
